2573c19c6ce8503380213479945c1ab167867381
Elem <elem15@yandex.ru>
2023-07-13 15:22:28 +0300

    feat: use auth with userService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,73 +5,53 @@ import { AuthDto } from './dto/auth.dto';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import * as argon2 from 'argon2';
 import { UserService } from '../user/user.service';
-import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    // private userService: UserService,
-    private prisma: PrismaService,
+    private userService: UserService,
     private jwtService: JwtService,
     private configService: ConfigService,
   ) { }
   async signUp(createUserDto: CreateUserDto): Promise<any> {
     // Check if user exists
-    const userExists = await this.findByEmail(createUserDto.email);
+    const userExists = await this.userService.findByEmail(createUserDto.email);
     if (userExists) {
       throw new BadRequestException('User already exists');
     }
 
     const password = await argon2.hash(createUserDto.password);
 
-    // const newUser = await this.userService.create({
-    //   ...createUserDto,
-    //   password,
-    // });
-    const newUser = await this.prisma.user.create({
-      data: {
-        ...createUserDto,
-        password,
-      },
+    const newUser = await this.userService.create({
+      ...createUserDto,
+      password,
     });
     const tokens = await this.getTokens(newUser.id, newUser.email);
-    // await this.updateRefreshToken(newUser.id, tokens.refreshToken);
+    await this.updateRefreshToken(newUser.id, tokens.refreshToken);
     return tokens;
   }
 
   async signIn(data: AuthDto) {
     // Check if user exists
-    const user = await this.findByEmail(data.email);
+    const user = await this.userService.findByEmail(data.email);
     if (!user) throw new BadRequestException('User does not exist');
     const passwordMatches = await argon2.verify(user.password, data.password);
     if (!passwordMatches)
       throw new BadRequestException('Password is incorrect');
     const tokens = await this.getTokens(user.id, user.email);
-    // await this.updateRefreshToken(user.id, tokens.refreshToken);
+    await this.updateRefreshToken(user.id, tokens.refreshToken);
     return tokens;
   }
 
-  // async logout(userId: number) {
-  //   return this.userService.update(userId, { refreshToken: null });
-  // }
+  async logout(userId: number) {
+    return this.userService.update(userId, { refreshToken: null });
+  }
 
-  // async updateRefreshToken(userId: number, refreshToken: string) {
-  //   const hashedRefreshToken = await argon2.hash(refreshToken);
-  //   await this.userService.update(userId, {
-  //     refreshToken: hashedRefreshToken,
-  //   });
-  // }
-
-  public async findByEmail(email: string): Promise<CreateUserDto> {
-    try {
-      const user = await this.prisma.user.findFirst({
-        where: { email },
-      });
-      return user;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
+  async updateRefreshToken(userId: number, refreshToken: string) {
+    const hashedRefreshToken = await argon2.hash(refreshToken);
+    await this.userService.update(userId, {
+      refreshToken: hashedRefreshToken,
+    });
   }
 
   async getTokens(userId: number, username: string) {
